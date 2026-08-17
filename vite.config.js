a0fa96545d619ad85115cea2e587ef8c30f51012
hWacha <56476426+hWacha2@users.noreply.github.com>
2026-08-17 21:21:41 +0900

    апдейт

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // для GitHub Pages
+  base: '/hwacha2.github.io/', // ← ВАЖНО: имя вашего репозитория
 })
